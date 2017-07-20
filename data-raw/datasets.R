b25fe2924e317f0d6b86e651fa919013a588d11a
sborms <borms_sam@hotmail.com>
2017-07-20 09:52:02 +0200

    restructure
    Former-commit-id: 326ca5edfbeb8fe118235cddd6c5ee0944eb0235

---
+++ data-raw/datasets.R
@@ -0,0 +1,218 @@
+
+############################################################
+################## Data sets manipulation ##################
+############################################################
+
+######################### US ECONOMY TEXTS CORPUS
+
+useconomy <- readr::read_csv("data-raw/US_economic_news_1951-2014.csv")
+useconomy$text <- stringi::stri_replace_all(useconomy$text, replacement = " ", regex = "</br></br>")
+useconomy$text <- stringi::stri_replace_all(useconomy$text, replacement = "", regex = '[\\"]')
+
+# TODO: further text cleaning
+# TODO: drop < 1970
+
+months <- lapply(stringi::stri_split(useconomy$date, regex = "/"), "[", 1)
+days <- lapply(stringi::stri_split(useconomy$date, regex = "/"), "[", 2)
+years <- lapply(stringi::stri_split(useconomy$date, regex = "/"), "[", 3)
+yearsLong <- lapply(years, function(x) if (as.numeric(x) > 14) return(paste0("19", x)) else return(paste0("20", x)))
+
+datesNew <- paste0(paste0(unlist(months), "/"), paste0(unlist(days), "/"), unlist(yearsLong))
+useconomy$dateNew <- datesNew
+
+dates <- as.character(as.Date(useconomy$dateNew, format = "%m/%d/%Y")) # character date is input requirement
+useconomy$dateNewDate <- dates
+
+freqDays <- plyr::count(dates)
+freqMonths <- plyr::count(unlist(lapply(stringi::stri_split(dates, regex = "-"),
+                                        function(x) return(paste0(x[1:2], collapse = "/")))))
+freqYears <- plyr::count(unlist(lapply(stringi::stri_split(dates, regex = "-"), "[", 1)))
+
+USECONOMYNEWS <- useconomy
+
+USECONOMYNEWS$date <- USECONOMYNEWS$dateNewDate
+
+source <- sapply(stringi::stri_split(USECONOMYNEWS$articleid, regex = "_"), "[", 1)
+wsj <- ifelse(source == "wsj", 1, 0) # wall street journal
+wapo <- abs(wsj - 1) # washington post
+USECONOMYNEWS$wsj <- wsj
+USECONOMYNEWS$wapo <- wapo
+
+economy <- ifelse(USECONOMYNEWS$relevance == "yes", 1, 0)
+noneconomy <- abs(economy - 1)
+USECONOMYNEWS$economy <- economy
+USECONOMYNEWS$noneconomy <- noneconomy
+
+# delete obsolete columns
+USECONOMYNEWS$dateNewDate <- USECONOMYNEWS$dateNew <- USECONOMYNEWS$`_last_judgment_at` <-
+USECONOMYNEWS$`_trusted_judgments` <- USECONOMYNEWS$`positivity:confidence` <- USECONOMYNEWS$`relevance:confidence` <-
+USECONOMYNEWS$relevance_gold <- USECONOMYNEWS$articleid <- USECONOMYNEWS$`_unit_state` <- USECONOMYNEWS$`_golden` <-
+USECONOMYNEWS$positivity_gold <- USECONOMYNEWS$relevance <- USECONOMYNEWS$positivity <- NULL
+
+USECONOMYNEWS <- as.data.table(USECONOMYNEWS)
+USECONOMYNEWS <- USECONOMYNEWS[order(date)]
+colnames(USECONOMYNEWS)[1] <- "id"
+
+save(USECONOMYNEWS, file = "DATA/USECONOMYNEWS.rda")
+# load("DATA/USECONOMYNEWS.rda")
+
+######################### S&P500 Index (1988-2014, monthly)
+
+sp500 <- readr::read_csv("data-raw/S&P500.csv")
+sp500 <- as.data.frame(sp500[c("Date", "Adj Close")])
+sp500 <- sp500[sp500$Date < "2014-12-31", ]
+sp500 <- xts::xts(sp500$`Adj Close`, sp500$Date)
+SP500 <- PerformanceAnalytics::Return.calculate(sp500)[-1, ] # returns
+
+save(SP500, file = "DATA/SP500.rda")
+
+######################### LEXICONS
+
+# LM csv file reordering
+lmRaw <- read.csv("data-raw/lexicons-raw/LM_raw.csv", sep = ";")
+lmPOS <- lmRaw[lmRaw$Positive != 0, ][, c("Word", "Positive")]
+lmPOS$Polarity <- 1
+lmPOS$Positive <- NULL
+lmNEG <- lmRaw[lmRaw$Negative != 0, ][, c("Word", "Negative")]
+lmNEG$Polarity <- -1
+lmNEG$Negative <- NULL
+lm <- rbind(lmPOS, lmNEG)
+write.csv2(lm, file = "data-raw/lexicons-raw/LM.csv", row.names = FALSE)
+# read.csv("data-raw/lexicons-raw/LM.csv")
+
+# FEEL csv file reordering
+feelRaw <- read.csv("data-raw/lexicons-raw/FEEL_raw.csv", sep = ";", encoding = "UTF-8")
+feelPOS <- feelRaw[feelRaw$polarity == "positive", ][, c("word", "polarity")]
+feelPOS$polarity <- 1
+feelNEG <- feelRaw[feelRaw$polarity == "negative", ][, c("word", "polarity")]
+feelNEG$polarity <- -1
+feel <- rbind(feelPOS, feelNEG)
+write.csv2(feel, file = "data-raw/lexicons-raw/FEEL.csv", row.names = FALSE)
+# read.csv("data-raw/lexicons-raw/FEEL.csv")
+
+# FEEL NL translation restructuring
+f <- read.csv("data-raw/lexicons-raw/FEEL_nl.csv", sep = ";")
+w <- as.character(f$word[f$word != ""])
+p <- f$polarity[!is.na(f$polarity)]
+new <- data.frame(word = w, polarity = p)
+write.csv2(new, file = "data-raw/lexicons-raw/FEEL_nl.csv", row.names = FALSE)
+
+# FEEL ENG translation restructuring
+f <- read.csv("data-raw/lexicons-raw/FEEL_eng.csv", sep = ";")
+w <- as.character(f$word[f$word != ""])
+p <- f$polarity[!is.na(f$polarity)]
+new <- data.frame(word = w, polarity = p)
+write.csv2(new, file = "data-raw/lexicons-raw/FEEL_eng.csv", row.names = FALSE)
+
+# GI csv file reordering
+giRaw <- read.csv("data-raw/lexicons-raw/GI_raw.csv", sep = ";")
+giPOS <- giRaw[giRaw$Positiv == "Positiv", ][, c("Entry", "Positiv")]
+giPOS$Polarity <- 1
+giPOS$Positiv <- NULL
+giNEG <- giRaw[giRaw$Negativ == "Negativ", ][, c("Entry", "Negativ")]
+giNEG$Polarity <- -1
+giNEG$Negativ <- NULL
+gi <- rbind(giPOS, giNEG)
+write.csv2(gi, file = "data-raw/lexicons-raw/GI.csv", row.names = FALSE)
+# read.csv("data-raw/lexicons-raw/GI.csv")
+
+prepare_word_list <- function(fileName, type, name, polarity = FALSE) {
+
+  w <- read.csv(paste0("data-raw/", type, "-raw/", fileName), sep = ";")
+
+  if (polarity == 1) w$y <- 1
+  else if (polarity == -1) w$y <- -1
+  colnames(w) <- c("x", "y")
+
+  w$x <- as.character(w$x)
+  w$x <- stringr::str_to_lower(w$x)
+  w$x <- stringr::str_trim(w$x)
+  w$x <- stringi::stri_replace_all(w$x, "", regex = '\\?') # remove question markets
+  w$x <- stringi::stri_replace_all(w$x, "", regex = '[\\"]') # remove double quotes
+  w$x <- stringi::stri_replace_all(w$x, "", regex = '[()]') # remove single brackets
+  w$x <- stringi::stri_replace_all(w$x, "", regex = '[@]') # remove @ character
+  w$y <- as.numeric(w$y)
+
+  w <- w[!duplicated(w$x), ]
+  w <- sentimentr::as_key(w, comparison = NULL) # makes absolutely sure duplicated words are removed
+  w <- w[w$x != "" & w$x != " " & w$x != "#naam", ]
+
+  fileOut <- paste0("data-raw/", name, ".rda")
+
+  return(list(w = w, file = fileOut))
+}
+
+# save lexicons with appropriate names to data-raw/ folder
+typeL <- "lexicons"
+l <- prepare_word_list("LM.csv", typeL, "LEXICON_LM_ENG"); "LEXICON_LM_ENG" <- l$w
+save(LEXICON_LM_ENG, file = l$file)
+l <- prepare_word_list("LM_fr.csv", typeL, "LEXICON_LM_FR_tr"); "LEXICON_LM_FR_tr" <- l$w
+save(LEXICON_LM_FR_tr, file = l$file)
+l <- prepare_word_list("LM_nl.csv", typeL, "LEXICON_LM_NL_tr"); "LEXICON_LM_NL_tr" <- l$w
+save(LEXICON_LM_NL_tr, file = l$file)
+l <- prepare_word_list("FEEL.csv", typeL, "LEXICON_FEEL_FR"); "LEXICON_FEEL_FR" <- l$w
+save(LEXICON_FEEL_FR, file = l$file)
+l <- prepare_word_list("FEEL_nl.csv", typeL, "LEXICON_FEEL_NL_tr"); "LEXICON_FEEL_NL_tr" <- l$w
+save(LEXICON_FEEL_NL_tr, file = l$file)
+l <- prepare_word_list("FEEL_eng.csv", typeL, "LEXICON_FEEL_ENG_tr"); "LEXICON_FEEL_ENG_tr" <- l$w
+save(LEXICON_FEEL_ENG_tr, file = l$file)
+l <- prepare_word_list("GI.csv", typeL,"LEXICON_GI_ENG"); "LEXICON_GI_ENG" <- l$w
+save(LEXICON_GI_ENG, file = l$file)
+l <- prepare_word_list("GI_fr.csv", typeL, "LEXICON_GI_FR_tr"); "LEXICON_GI_FR_tr" <- l$w
+save(LEXICON_GI_FR_tr, file = l$file)
+l <- prepare_word_list("GI_nl.csv", typeL, "LEXICON_GI_NL_tr"); "LEXICON_GI_NL_tr" <- l$w
+save(LEXICON_GI_NL_tr, file = l$file)
+l <- prepare_word_list("HENRY.csv", typeL, "LEXICON_HENRY_ENG"); "LEXICON_HENRY_ENG" <- l$w
+save(LEXICON_HENRY_ENG, file = l$file)
+l <- prepare_word_list("HENRY_fr.csv", typeL, "LEXICON_HENRY_FR_tr"); "LEXICON_HENRY_FR_tr" <- l$w
+save(LEXICON_HENRY_FR_tr, file = l$file)
+l <- prepare_word_list("HENRY_nl.csv", typeL, "LEXICON_HENRY_NL_tr"); "LEXICON_HENRY_NL_tr" <- l$w
+save(LEXICON_HENRY_NL_tr, file = l$file)
+
+# creates and places LEXICON data in DATA folder
+form_word_list <- function(type) {
+
+  if (type == "LEXICON") pattern <- c("LEXICON_")
+  else if (type == "VALENCE") pattern <- c("NEGATORS_") # change if additional valence shifter categories are added
+
+  objects <- list.files("data-raw/", pattern = pattern)
+
+  listed <- vector(mode = "list")
+  for (o in 1:length(objects)) {
+    obj <- load(paste0("data-raw/", objects[o])) # name
+    toAdd <- eval(parse(text = obj)) # object
+    listed[[obj]] <- toAdd
+    cat("added:", obj, "\n")
+  }
+  if (type == "LEXICON") {
+    assign("LEXICONS", value = listed, pos = 1)
+    save(LEXICONS, file = "DATA/LEXICONS.rda")
+  }
+  else if (type == "VALENCE") {
+    assign("VALENCE", value = listed, pos = 1)
+    save(VALENCE, file = "DATA/VALENCE.rda")
+  }
+}
+
+form_word_list(type = "LEXICON")
+# load("DATA/LEXICONS.rda")
+
+######################### VALENCE WORD LISTS
+
+# get negators from lexicon package
+negators <- lexicon::hash_valence_shifters[y == 1]
+negators$y <- as.numeric(negators$y)
+negators$y <- -1
+write.csv2(negators, file = "data-raw/valence-raw/NEGATORS.csv", row.names = FALSE)
+
+typeV <- "valence"
+v <- prepare_word_list("NEGATORS.csv", typeV, "NEGATORS_ENG"); "NEGATORS_ENG" <- v$w
+save(NEGATORS_ENG, file = v$file)
+v <- prepare_word_list("NEGATORS_fr.csv", typeV, "NEGATORS_FR_tr"); "NEGATORS_FR_tr" <- v$w
+save(NEGATORS_FR_tr, file = v$file)
+v <- prepare_word_list("NEGATORS_nl.csv", typeV, "NEGATORS_NL_tr"); "NEGATORS_NL_tr" <- v$w
+save(NEGATORS_NL_tr, file = v$file)
+
+form_word_list(type = "VALENCE")
+# load("DATA/VALENCE.rda")
+
--- tests/informal_testing.R
+++ tests/informal_testing.R
@@ -1,5 +1,7 @@
 
-### informal testing ###
+############################################################
+##################### Informal testing #####################
+############################################################
 
 require(plyr)
 require(dplyr)
@@ -8,13 +10,13 @@ require(data.table)
 require(zoo)
 
 str <- c("./R/")
-fileSources <- list.files(str, pattern = "*.R$", full.names = TRUE, ignore.case = TRUE)
+sources <- list.files(str, pattern = "*.R$", full.names = TRUE, ignore.case = TRUE)
 
-sapply(fileSources[fileSources != "./R/_sentiment_calculation_example.R" & fileSources != "./R/sentmodel.R"], source, .GlobalEnv)
+sapply(sources, source, .GlobalEnv)
 
-load("DATA/test_corpus0.rda")
+load("DATA/belga.rda") # BELGA 2016 news data
 
-tester <- test_corpus0$fr # unordered by date
+tester <- belga$fr # unordered by date
 
 categs <- unique(tester$category)
 for (el in categs) {
@@ -29,19 +31,21 @@ tester2 <- tester[, c("id", "date", "body", categs[categs != ""])] # drop langua
 tester2$date <- as.Date(tester2$date, format = "%d/%m/%Y")
 names(tester2)[names(tester2) == "body"] <- "text"
 
-lexs <- c("French_FEEL_lexicon.csv", "French_McDonald_GT_lexicon.csv")
-lexicons <- lapply(lexs, function(x) return(read.csv(paste0("LEXICON/", x), sep = ";")))
-names(lexicons) <- gsub("_lexicon.csv", "", lexs)
+lexs <- c("FEEL.csv", "LM_fr.csv")
+lexicons <- lapply(lexs, function(x) return(read.csv(paste0("data-raw/lexicons-raw/", x), sep = ";")))
+names(lexicons) <- gsub(".csv", "", lexs)
 
 lexicons <- lapply(lexicons, function(x) {x[, 1] <- stringr::str_to_lower(x[, 1]); return(x)})
 lexicons <- lapply(lexicons, function(x) {x[, 1] <- stringr::str_trim(x[, 1]); return(x)})
-# lexicons <- lapply(lexicons, function(x) {x[!(stri_detect_fixed(l[, 1], " ")), ]}) # keep only single words
+# lexicons <- lapply(lexicons, function(x) {x[!(stringi::stri_detect_fixed(l[, 1], " ")), ]}) # keep only single words
 
-valence <- read.csv("LEXICON/valence_French.csv", sep = ";")
+# lexicons <- LEXICONS[c("LEXICON_FEEL_FR", "LEXICON_MCDONALD_FR_tr")]
+
+valence <- read.csv("data-raw/valence-raw/NEGATORS_fr.csv", sep = ";")
 
 ###################################################################################################
 
-cAll <- corpuS(tester2)
+cAll <- sento_corpus(tester2)
 
 cPol <- corpus_subset(cAll, POL == 1) # quanteda
 cGen <- corpus_subset(cAll, GEN == 1 | GENERAL == 1 | ALG == 1) # quanteda
@@ -49,8 +53,9 @@ cGen <- corpus_subset(cAll, GEN == 1 | GENERAL == 1 | ALG == 1) # quanteda
 c <- corpus_sample(cAll, size = 500) # quanteda
 summary(c) # quanteda
 
-lexiconsIn <- setup_lexicons(lexicons, valence)
-lexiconsIn2 <- setup_lexicons(lexicons, valence, split = TRUE)
+lexiconsIn <- setup_lexicons(lexicons = c("LEXICON_FEEL_FR", "LEXICON_LM_FR_tr"))
+lexiconsIn2 <- setup_lexicons(lexicons, valence)
+lexiconsIn3 <- setup_lexicons(lexicons, valence, do.split = TRUE)
 
 ###
 
@@ -64,7 +69,7 @@ lexiconsIn2 <- setup_lexicons(lexicons, valence, split = TRUE)
 
 # weights <- lexiconsIn$French_FEEL$y
 # names(weights) <- words
-# dfm_w2 <- dfm_weight(dfm, weights = weights) # weight of 1 for words not in lexicon, so need weights for which these are equal to 0
+# dfm_w2 <- dfm_weight(dfm, weights = weights) # weight of 1 for words not in lexicon, needs to be 0
 
 # head(dfm_select(dfm, features = words))
 
@@ -75,17 +80,10 @@ sent2 <- compute_sentiment(c, lexiconsIn, how = "proportional")
 sent3 <- compute_sentiment(c, lexiconsIn, how = "tf-idf")
 sent4 <- compute_sentiment(c, lexiconsIn, how = "counts")
 
-aggDocs1 <- agg_documents(sent1, how = "equal-weight", ignoreZeros = FALSE)
-# aggDocs1$sent
-
-aggDocs2 <- agg_documents(sent1, how = "proportional")
-# aggDocs2$sent
-
-aggDocs3 <- agg_documents(sent1, how = "equal-weight", ignoreZeros = FALSE)
-# aggDocs3$sent
-
-aggDocs4 <- agg_documents(sent1, how = "proportional", ignoreZeros = TRUE)
-# aggDocs4$sent
+aggDocs1 <- agg_documents(sent1, by = "day", how = "equal-weight", do.ignoreZeros = FALSE)
+aggDocs2 <- agg_documents(sent1, by = "week", how = "proportional")
+aggDocs3 <- agg_documents(sent1, by = "month", how = "equal-weight", do.ignoreZeros = FALSE)
+aggDocs4 <- agg_documents(sent1, by = "year", how = "proportional", do.ignoreZeros = TRUE)
 
 # al <- almons(n = 3, orders = 1:3, inverse = TRUE); al
 # al2 <- almons(n = 30, orders = 1:5, inverse = TRUE); al2
@@ -95,37 +93,51 @@ aggDocs4 <- agg_documents(sent1, how = "proportional", ignoreZeros = TRUE)
 # w3 <- setup_time_weights(3, how = "linear"); w3
 # w4 <- setup_time_weights(3, how = "exponential", alphas = c(0.01, 0.1, 0.4, 0.7)); w4
 
-sentMeas1 <- agg_time(aggDocs1, 3, how = "almon", list(orders = 1:3, inverse = TRUE, normalize = TRUE))
-# sentMeas1
-sentMeas2 <- agg_time(aggDocs1, 3, how = "equal-weight")
-# sentMeas2
+sentMeas1 <- agg_time(aggDocs1, lag = 10, how = "almon", list(orders = 1:3, do.inverse = TRUE, do.normalize = TRUE))
+sentMeas2 <- agg_time(aggDocs2, 3, how = "equal-weight")
+sentMeas3 <- agg_time(aggDocs3, 3, how = "exponential", list(alphas = seq(0.1, 0.5, by = 0.1)))
+sentMeas4 <- agg_time(aggDocs4, 1, how = "equal-weight")
+sentMeas5 <- agg_time(aggDocs1, lag = NULL, how = "own", list(weights = data.frame(w1 = c(0.5, 0.3, 0.2),
+                                                                                   w2 = c(0.3, 0.3, 0.4))))
 
-ctrlM1 <- ctrl_merge(sentMeas1,
-                     lex = list(LEX = c("French_FEEL", "French_McDonald_GT")),
-                     feat = list(GEN = c("GEN", "GENERAL"), FEAT2 = c("ECO", "POL")),
-                     time = list(W1 = c("almon1", "almon1_inv"), W2 = c("almon2", "almon3")),
-                     keep = FALSE)
+ctrM1 <- ctr_merge(sentMeas1,
+                   lex = list(LEX = c("LEXICON_FEEL_FR", "LEXICON_LM_FR_tr")),
+                   feat = list(GEN = c("GEN", "GENERAL"), FEAT2 = c("ECO", "POL")),
+                   time = list(W1 = c("almon1", "almon1_inv"), W2 = c("almon2", "almon3")),
+                   do.keep = FALSE)
 
-ctrlM2 <- ctrl_merge(sentMeas1,
-                     lex = list(LEX = c("French_FEEL", "French_McDonald_GT")),
-                     feat = list(WRONG3 = c("Oops")),
-                     time = list(WRONG1 = c("almon1", "almon1"), WRONG2 = c("almon1_INV")),
-                     keep = TRUE) # produces error and warnings as it should
+ctrM2 <- ctr_merge(sentMeas1,
+                   lex = list(LEX = c("LEXICON_FEEL_FR", "LEXICON_LM_FR_tr")),
+                   feat = list(WRONG3 = c("Oops")),
+                   time = list(WRONG1 = c("almon1", "almon1"), WRONG2 = c("almon1_INV")),
+                   do.keep = TRUE) # produces error and warnings as it should
 
-m1 <- merge_measures(ctrlM1)
+m1 <- merge_measures(ctrM1)
 
-ctrlAgg <- ctrl_agg(howWithin = "equal-weight",
-                    howDocs = "equal-weight",
-                    howTime = "almon",
-                    ignoreZeros = FALSE,
-                    lag = 3,
-                    orders = 1:3,
-                    inverse = TRUE,
-                    normalize = TRUE)
+ctrAgg <- ctr_agg(howWithin = "equal-weight",
+                  howDocs = "equal-weight",
+                  howTime = "almon",
+                  do.ignoreZeros = FALSE,
+                  lag = 3,
+                  by = "month",
+                  orders = 1:3,
+                  do.inverse = TRUE,
+                  do.normalize = TRUE)
 
-sentMeas3 <- perform_agg(sent1, ctrlAgg)
+ctrAgg2 <- ctr_agg(howWithin = "equal-weight",
+                  howDocs = "equal-weight",
+                  howTime = "equal-weight", # automatically set to "own"
+                  do.ignoreZeros = FALSE,
+                  by = "week",
+                  orders = 1:3,
+                  do.inverse = TRUE,
+                  do.normalize = TRUE,
+                  weights = data.frame(w1 = c(0.5, 0.3, 0.2),
+                                       w2 = c(0.3, 0.3, 0.4)))
 
-sentMeas4 <- sentmeasures(c, lexiconsIn, ctrlAgg)
+sentMeas6 <- perform_agg(sent1, ctrAgg)
+sentMeas7 <- sento_measures(c, lexiconsIn, ctrAgg)
+sentMeas8 <- perform_agg(sent1, ctrAgg2)
 
 # sentMeas1$measures == sentMeas3$measures  # should be all TRUE
 # sentMeas3$measures == sentMeas4$out$measures # should be all TRUE
@@ -133,82 +145,39 @@ sentMeas4 <- sentmeasures(c, lexiconsIn, ctrlAgg)
 mSel <- select_measures(m1, c("LEX", "FEAT2"))
 # mSel <- select_measures(m1, c("LEX", "not_in_here", "wrong_again")) # produces error as it should
 
+plot(sentMeas2)
+plot(m1)
+plot(mSel)
+
+fill1 <- fill_measures(sentMeas2, do.fillLatest = FALSE)
+
 modeling <- FALSE
 if (modeling) {
-  nSample <- 350
+  nSample <- 348
   start <- 3
   oos <- 4
   h <- 3
   alphas <- seq(0, 1, by = 0.20)
   lambdas <- 10^seq(2, -2, length.out = 50)
 
-  sentmeasures <- sentMeas1
-  sentmeasures <- fill_measures(sentmeasures)
+  sentomeasures <- sentMeas1
+  sentomeasures <- fill_measures(sentomeasures)
 
-  y <- data.frame(y = 0.01 + runif(nrow(sentmeasures$measures), 0.01, 0.04))
+  y <- data.frame(y = 0.01 + runif(nrow(sentomeasures$measures), 0.01, 0.04))
   x <- data.frame(xOther = y$y)
-  row.names(y) <- row.names(sentmeasures$measures) # row.names (dates) shouldn't be necessary, but assumes nrows are aligned
-  trainWindow <- 340
+  trainWindow <- 338
 
-  ctrlModel1 <- ctrl_model(model = "lm", type = "BIC", iter = FALSE, h = h, alphas = alphas, lambdas = lambdas)
+  ctrModel1 <- ctr_model(model = "lm", type = "BIC", do.iter = FALSE, h = h, alphas = alphas, lambdas = lambdas)
+  bicTest <- sento_lm(sentomeasures, y, x, ctrModel1)
 
-  bicTest <- lm_sent(sentmeasures, y, x, ctrlModel1)
+  ctrModel2 <- ctr_model(model = "lm", type = "cv", do.iter = FALSE, h = h, alphas = alphas, lambdas = lambdas,
+                         oos = oos, trainWindow = trainWindow)
+  cvTest <- sento_lm(sentomeasures, y, x, ctrModel2)
 
-  ctrlModel2 <- ctrl_model(model = "lm", type = "cv", iter = FALSE, h = h, alphas = alphas, lambdas = lambdas,
-                          oos = oos, trainWindow = trainWindow)
-
-  cvTest <- lm_sent(sentmeasures, y, x, ctrlModel2)
-
-  ctrlModel3 <- ctrl_model(model = "lm", type = "BIC", iter = TRUE, h = h, alphas = alphas, lambdas = lambdas,
-                           nSample = nSample, start = start, oos = oos, trainWindow = NULL)
-
-  bicTestIter <- lm_sent(sentmeasures, y, x, ctrlModel3)
+  ctrModel3 <- ctr_model(model = "lm", type = "BIC", do.iter = TRUE, h = h, alphas = alphas, lambdas = lambdas,
+                         nSample = nSample, start = start, oos = oos, trainWindow = NULL)
+  bicTestIter <- sento_lm(sentomeasures, y, x, ctrModel3)
 
   ### test cvTestIter + bicTestIter to check
 }
-
-###
-
-example <- FALSE
-if (example) {
-  cAll <- corpuS(text_df)
-  c <- corpus_sample(cAll, size = 500) # quanteda
-
-  lexiconsIn <- setup_lexicons(lexicons, valence)
-
-  # step-by-step
-  sent <- compute_sentiment(c, lexiconsIn, how = "tf-idf")
-
-  aggDocs <- agg_documents(sent, how = "equal-weight", ignoreZeros = FALSE)
-
-  sentMeas <- agg_time(aggDocs, lag = 3, how = "almon", list(orders = 1:3, inverse = TRUE, normalize = TRUE))
-
-  # via perform_agg()
-  ctrlAgg <- ctrl_agg(howWithin = "tf-idf",
-                      howDocs = "equal-weight",
-                      howTime = "almon",
-                      ignoreZeros = FALSE,
-                      lag = 3,
-                      orders = 1:3,
-                      inverse = TRUE,
-                      normalize = TRUE)
-
-  sentMeas2 <- perform_agg(sent, ctrlAgg)
-
-  # compute_sentiment() + perform_agg()
-  sentMeas3 <- sentmeasures(c, lexiconsIn, ctrlAgg)
-
-  # merge sentiment measures/indices
-  ctrlMerge <- ctrl_merge(sentMeas,
-                          lex = list(LEX = c("French_FEEL", "French_McDonald_GT")),
-                          feat = list(GEN = c("GEN", "GENERAL"), FEAT2 = c("ECO", "POL")),
-                          time = list(W1 = c("almon1", "almon1_inv"), W2 = c("almon2", "almon3")),
-                          keep = FALSE)
-
-  sentMeasMerged <- merge_measures(ctrlMerge)
-
-  mSel <- select_measures(sentMeasMerged, c("LEX", "FEAT2"))
-}
-
-###
 
