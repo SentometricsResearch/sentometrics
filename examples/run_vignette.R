0b5ae967958160e2f86585a64014017a4a491c1c
Samuel Borms <borms_sam@hotmail.com>
2018-12-23 18:03:48 +0100

    modified examples
    Former-commit-id: 218c43a1958e05266b3912aa2b316c6465fde462

--- examples/run_vignette.R
+++ examples/run_vignette.R
@@ -1,31 +1,48 @@
 
-###########################################
-############## VIGNETTE CODE ##############
-###########################################
+###############################
+#######  VIGNETTE CODE  #######  
+###############################
 
 ###### DESCRIPTION ######
 
 ### This code is used in the vignette paper 'The R Package sentometrics to Compute,
-### Aggregate and Predict with Textual Sentiment' (Ardia, Bluteau, Borms and Boudt, 2017)
+### Aggregate and Predict with Textual Sentiment' (Ardia, Bluteau, Borms and Boudt, 2018)
 ### See the paper for the results and setup details
 ### Download the package first before you run this script...
-#### install.packages("sentometrics") # from CRAN (version 0.5.5), OR
-#### install.packages("sentometrics_0.5.5.tar.gz", repos = NULL) # from the tar
+### install.packages("sentometrics") # from CRAN (version 0.5.6), OR
+### install.packages("sentometrics_0.5.6.tar.gz", repos = NULL) # from the tar
+
+###### SESSION INFO ######
+
+### R version 3.5.1 (2018-07-02)
+### Platform: x86_64-w64-mingw32/x64 (64-bit)
+### Running under: Windows >= 8 x64 (build 9200)
+###  
+### Matrix products: default
+###  
+### locale:
+### [1] LC_COLLATE=Dutch_Belgium.1252  LC_CTYPE=Dutch_Belgium.1252    LC_MONETARY=Dutch_Belgium.1252
+### [4] LC_NUMERIC=C                   LC_TIME=Dutch_Belgium.1252    
+###  
+### attached base packages:
+### [1] stats     graphics  grDevices utils     datasets  methods   base     
+###  
+### other attached packages:
+### [1] lubridate_1.7.4    stm_1.3.3          quanteda_1.3.14    lexicon_1.1.3      gridExtra_2.3      ggplot2_3.1.0     
+### [7] sentometrics_0.5.6 data.table_1.11.8 
+###  
+### loaded via a namespace (and not attached):
+### [1] Rcpp_1.0.0         pillar_1.3.1       compiler_3.5.1     plyr_1.8.4         bindr_0.1.1        iterators_1.0.10  
+### [7] tools_3.5.1        stopwords_0.9.0    tibble_1.4.2       gtable_0.2.0       lattice_0.20-38    pkgconfig_2.0.2   
+### [13] rlang_0.3.0.1      Matrix_1.2-15      foreach_1.4.4      fastmatch_1.1-0    yaml_2.2.0         bindrcpp_0.2.2    
+### [19] withr_2.1.2        dplyr_0.7.8        stringr_1.3.1      syuzhet_1.0.4      grid_3.5.1         glmnet_2.0-16     
+### [25] tidyselect_0.2.5   glue_1.3.0         R6_2.3.0           purrr_0.2.5        spacyr_1.0         magrittr_1.5      
+### [31] scales_1.0.0       codetools_0.2-15   assertthat_0.2.0   colorspace_1.3-2   stringi_1.2.4      RcppParallel_4.4.2
+### [37] lazyeval_0.2.1     munsell_0.5.0      crayon_1.3.4
 
 remove(list = ls())
 options(prompt = "R> ", continue = "+  ", width = 120, digits = 4, max.print = 80, useFancyQuotes = FALSE)
 sink(file = "output_vignette.txt", append = FALSE, split = TRUE) # output printed in .txt file
-
-info <- sessionInfo()
-cat(info$R.version$version.string, "\n")
-cat(info$platform, "\n")
-cat(info$locale, "\n \n")
-
-##################
-###### CODE ######
-##################
-
-cat("### SECTION 3.1 ####################### \n \n")
 
 library("sentometrics")
 
@@ -35,6 +52,19 @@ library("lexicon")
 library("quanteda")
 library("stm")
 library("lubridate")
+
+info <- sessionInfo()
+cat(info$locale, "\n \n")
+print(info)
+cat("\n")
+
+##################
+###### CODE ######
+##################
+
+cat("### SECTION 3.1 ####################### \n \n")
+
+library("sentometrics")
 
 data("usnews", package = "sentometrics")
 class(usnews)
