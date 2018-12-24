d520514a7bbb6da5899a331d89cdd646471e0f23
Samuel Borms <borms_sam@hotmail.com>
2018-12-24 10:43:51 +0100

    modified examples

--- examples/run_timings.R
+++ examples/run_timings.R
@@ -3,7 +3,7 @@
 #######  COMPARISON OF TEXTUAL SENTIMENT COMPUTATIONS  #######
 ##############################################################
 
-###### DESCRIPTION ###### 
+###### DESCRIPTION ######
 
 ### This code is used in the vignette paper 'The R Package sentometrics to Compute,
 ### Aggregate and Predict with Textual Sentiment' (Ardia, Bluteau, Borms and Boudt, 2018)
@@ -14,39 +14,39 @@
 
 ###### WARNING ######
 
-### For a corpus of 100,000 texts, we run the SentimentAnalysis function
-### separately (cf. timingsSentimentAnalysis <- ...) to avoid a memory error; 
-### it might be that this memory error also occurs for a smaller corpus size
+### We run the SentimentAnalysis function separately (cf. timingsSentimentAnalysis <- ...)
+### to avoid a memory error for a corpus of 100,000 texts
+### It might be that this memory error already occurs for a smaller corpus size
 
 ###### SESSION INFO ######
 
 ### R version 3.5.1 (2018-07-02)
 ### Platform: x86_64-w64-mingw32/x64 (64-bit)
 ### Running under: Windows >= 8 x64 (build 9200)
-###  
+###
 ### Matrix products: default
-###  
+###
 ### locale:
 ### [1] LC_COLLATE=Dutch_Belgium.1252  LC_CTYPE=Dutch_Belgium.1252    LC_MONETARY=Dutch_Belgium.1252
-### [4] LC_NUMERIC=C                   LC_TIME=Dutch_Belgium.1252    
-###  
+### [4] LC_NUMERIC=C                   LC_TIME=Dutch_Belgium.1252
+###
 ### attached base packages:
-### [1] stats     graphics  grDevices utils     datasets  methods   base     
-###  
+### [1] stats     graphics  grDevices utils     datasets  methods   base
+###
 ### other attached packages:
-### [1] microbenchmark_1.4-6    tidyr_0.8.2             dplyr_0.7.8             lexicon_1.1.3          
-### [5] SentimentAnalysis_1.3-2 syuzhet_1.0.4           meanr_0.1-1             tidytext_0.2.0         
-### [9] quanteda_1.3.14         sentometrics_0.5.6      data.table_1.11.8      
-###  
+### [1] microbenchmark_1.4-6    tidyr_0.8.2             dplyr_0.7.8             lexicon_1.1.3
+### [5] SentimentAnalysis_1.3-2 syuzhet_1.0.4           meanr_0.1-1             tidytext_0.2.0
+### [9] quanteda_1.3.14         sentometrics_0.5.6      data.table_1.11.8
+###
 ### loaded via a namespace (and not attached):
-### [1] Rcpp_1.0.0         pillar_1.3.1       compiler_3.5.1     plyr_1.8.4         bindr_0.1.1        tokenizers_0.2.1  
-### [7] iterators_1.0.10   tools_3.5.1        stopwords_0.9.0    nlme_3.1-137       lubridate_1.7.4    tibble_1.4.2      
-### [13] gtable_0.2.0       lattice_0.20-38    pkgconfig_2.0.2    rlang_0.3.0.1      Matrix_1.2-15      foreach_1.4.4     
-### [19] fastmatch_1.1-0    yaml_2.2.0         bindrcpp_0.2.2     janeaustenr_0.1.5  stringr_1.3.1      generics_0.0.2    
-### [25] grid_3.5.1         glmnet_2.0-16      tidyselect_0.2.5   glue_1.3.0         R6_2.3.0           ggplot2_3.1.0     
-### [31] purrr_0.2.5        spacyr_1.0         magrittr_1.5       backports_1.1.3    SnowballC_0.5.1    scales_1.0.0      
-### [37] codetools_0.2-15   assertthat_0.2.0   colorspace_1.3-2   stringi_1.2.4      RcppParallel_4.4.2 lazyeval_0.2.1    
-### [43] munsell_0.5.0      broom_0.5.1        crayon_1.3.4 
+### [1] Rcpp_1.0.0         pillar_1.3.1       compiler_3.5.1     plyr_1.8.4         bindr_0.1.1        tokenizers_0.2.1
+### [7] iterators_1.0.10   tools_3.5.1        stopwords_0.9.0    nlme_3.1-137       lubridate_1.7.4    tibble_1.4.2
+### [13] gtable_0.2.0       lattice_0.20-38    pkgconfig_2.0.2    rlang_0.3.0.1      Matrix_1.2-15      foreach_1.4.4
+### [19] fastmatch_1.1-0    yaml_2.2.0         bindrcpp_0.2.2     janeaustenr_0.1.5  stringr_1.3.1      generics_0.0.2
+### [25] grid_3.5.1         glmnet_2.0-16      tidyselect_0.2.5   glue_1.3.0         R6_2.3.0           ggplot2_3.1.0
+### [31] purrr_0.2.5        spacyr_1.0         magrittr_1.5       backports_1.1.3    SnowballC_0.5.1    scales_1.0.0
+### [37] codetools_0.2-15   assertthat_0.2.0   colorspace_1.3-2   stringi_1.2.4      RcppParallel_4.4.2 lazyeval_0.2.1
+### [43] munsell_0.5.0      broom_0.5.1        crayon_1.3.4
 
 remove(list = ls())
 
