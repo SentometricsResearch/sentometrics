2aa4d1ee386d3adb7fde37679c963af3fde023d0
sborms <samuel.borms@unine.ch>
2019-06-07 10:37:56 +0200

    new C++ code

--- src/compute_sentiment_valence.cpp
+++ src/compute_sentiment_valence.cpp
@@ -17,6 +17,7 @@ Rcpp::NumericMatrix compute_sentiment_valence(std::vector< std::vector<std::stri
 
   int nTexts = texts.size(); // already tokenized texts
   int nL = lexicons.size() - 1;
+  bool is_freq_weighting = is_frequency_weighting(how);
   Rcpp::CharacterVector colNames = prepare_column_names(lexicons.names(), nL);
 
   std::unordered_map< std::string, std::vector<double> > lexiconMap = make_lexicon_map(lexicons, nL);
@@ -24,13 +25,18 @@ Rcpp::NumericMatrix compute_sentiment_valence(std::vector< std::vector<std::stri
   Rcpp::List valenceList = lexicons[nL];
   Rcpp::CharacterVector valenceCols = valenceList.names();
   std::unordered_map< std::string, double > valenceMap = make_valence_map(valenceList);
+  std::unordered_map< int, std::unordered_map<std::string, double> > frequencyMap;
+  std::unordered_map< std::string, double > inverseFrequencyMap;
+  if (is_freq_weighting) {
+    make_frequency_maps(frequencyMap, inverseFrequencyMap, texts, how);
+  }
 
   Rcpp::NumericMatrix sentScores(nTexts, nL + 1); // output matrix of word count and sentiment scores
   if (valenceCols[1] == "y") {
-    SentimentScorerBigrams sentimentScorer(texts, lexiconMap, valenceMap, how, nL, sentScores);
+    SentimentScorerBigrams sentimentScorer(texts, lexiconMap, valenceMap, how, nL, frequencyMap, inverseFrequencyMap, is_freq_weighting, sentScores);
     parallelFor(0, nTexts, sentimentScorer);
   } else if (valenceCols[1] == "t") {
-    SentimentScorerClusters sentimentScorer(texts, lexiconMap, valenceMap, how, nL, sentScores);
+    SentimentScorerClusters sentimentScorer(texts, lexiconMap, valenceMap, how, nL, frequencyMap, inverseFrequencyMap, is_freq_weighting, sentScores);
     parallelFor(0, nTexts, sentimentScorer);
   }
 
